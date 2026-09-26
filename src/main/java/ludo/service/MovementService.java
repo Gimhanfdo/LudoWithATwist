98c503d34980120315bc1a561dcfe53667a29d89
Gimhan Fernando <gimhanfdo2004@gmail.com>
2026-09-26 22:35:57 +0530

    update MovementService

--- src/main/java/ludo/service/MovementService.java
+++ src/main/java/ludo/service/MovementService.java
@@ -9,11 +9,13 @@ public class MovementService {
 
     private final MoveExecutor moveExecutor;
     private final CaptureService captureService;
+    private final BlockService blockService;
     private final GameState gameState;
 
     public MovementService(
             MoveExecutor moveExecutor,
             CaptureService captureService,
+            BlockService blockService,
             GameState gameState) {
         if (moveExecutor == null) {
             throw new IllegalArgumentException("Move executor cannot be null.");
@@ -23,18 +25,29 @@ public class MovementService {
             throw new IllegalArgumentException("Capture service cannot be null.");
         }
 
+        if (blockService == null) {
+            throw new IllegalArgumentException("Block service cannot be null.");
+        }
+
         if (gameState == null) {
             throw new IllegalArgumentException("Game state cannot be null.");
         }
 
         this.moveExecutor = moveExecutor;
         this.captureService = captureService;
+        this.blockService = blockService;
         this.gameState = gameState;
     }
 
     public MovementResult moveOnStandardPath(Piece piece, int distance) {
+        
+        int allowedDistance = blockService.getAllowedMovementDistance(piece, distance);
 
-        boolean moved = moveExecutor.moveOnStandardPath(piece, distance);
+        if (allowedDistance == 0) {
+            return MovementResult.NOT_MOVED;
+        }
+
+        boolean moved = moveExecutor.moveOnStandardPath(piece, allowedDistance);
 
         if (!moved) {
             return MovementResult.NOT_MOVED;
